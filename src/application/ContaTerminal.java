f4c50dc45baef293ea4ac007c83d076a7d88a111
Arthur Moreira <arthurmoreirdagama@gmail.com>
2025-02-05 12:56:34 -0300

    Projeto Finalizado

--- src/application/ContaTerminal.java
+++ src/application/ContaTerminal.java
@@ -1,12 +1,34 @@
 package application;
 
+import java.util.Scanner;
 
 public class ContaTerminal {
 
 	public static void main(String[] args) {
 	
-		System.out.println("Hello World");
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Por favor, digite o numero");
+		int numero = sc.nextInt();
+		sc.nextLine();
+		
+		System.out.println("Por favor, digite a agencia");
+		String agencia = sc.nextLine();
+		
+		System.out.println("Por favor, digite o nome do cliente");
+		String nomeCliente = sc.nextLine();
+		
+		System.out.println("Por favor digite o saldo");
+		Double saldo = sc.nextDouble();
+		
+		System.out.println("Olá " 
+		+ nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " 
+		+ agencia + ", conta " 
+		+ numero + " e seu saldo " 
+		+ saldo + " já está disponível para saque");
+				
+		
+		sc.close();
 	}
 
 }
